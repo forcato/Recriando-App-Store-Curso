a4c9c785787081aa2e6993c9d89f196943dd887d
Lucas Forcato <lucasforcatoc@gmail.com>
2021-08-31 14:29:01 -0400

    Initial Commit

---
+++ Recriando App Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recriando App Store
+//
+//  Created by Lucas Forcato on 31/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
